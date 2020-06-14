9d0c414f4f757755f1fd07eb9eb7f41bbb42136b
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-14 16:21:43 -0500

    initial creation

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    /**
+     * The main page of the website
+     */
+    public function index()
+    {
+        return view('homepages.index');
+    }
+}
