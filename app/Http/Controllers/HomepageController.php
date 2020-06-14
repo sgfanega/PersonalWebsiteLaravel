a8b742cd600e358fbdfbbfd6625628cd91248308
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-14 15:24:52 -0500

    Added construct and index

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Homepage;
 
 class HomepageController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
+     * Display the homepage
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('homepage.index');
+    }
 }
