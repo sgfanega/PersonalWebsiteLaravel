be8b55bca0a4f7ef158655db83ab6d01da3f1df5
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-16 16:27:06 -0500

    Added Update Function()

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -29,5 +29,61 @@ class ResumeController extends Controller
 
         return view('resume.index')->with('resume', $resume[0]);
     }
-    
+
+    /**
+     * Show the form for editing the specified resource
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $resume = Resume::find($id);
+
+        if (auth()->user()->id !== 1) {
+            return redirect('/')->with('error', 'Unauthorized User.');
+        }
+
+        return view('resume.edit')->with('resume', $resume);
+    }
+
+    /**
+     * Update the specified resource in storage
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'pdf_source'=>'required|max:1999'
+        ]);
+
+        // Handle the file upload
+        if($request->hasFile('pdf_source')) {
+            // Get filename with extension
+            $fileNameWithExt = $request->file('pdf_source')->getClientOriginalName();
+            // Get filename
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            // Get extension
+            $fileExtension = $request->file('pdf_source')->getClientOriginalExtension();
+            // Throws error if the extension is not .pdf
+            if($fileExtension != 'pdf') {
+                return redirect('/resume/' . $id . '/edit')->with('error', "File is not a PDF");
+            }
+            // Filename to store
+            $fileNameToStore = $fileName . '_' . time() . '.' . $fileExtension;
+            // Upload the file
+            $path = $request->file('pdf_source')->storeAs('public/pdf_sources', $fileNameToStore);
+        }
+
+        $resume = Resume::find($id);
+        if($request->hasFile('pdf_source')) {
+            $resume->pdf_source = $fileNameToStore;
+        }
+        $resume->save();
+
+        return redirect('/resume')->with('success', 'Resume Updated');
+    }
 }
