534772ebf2280b9d4a053afeef6ebf4b03ecb4b3
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-16 00:36:58 -0500

    Removed Forloop

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,28 +1,26 @@
 @extends('layouts.app')
 @section('content')
 <div class="flex-container d-flex justify-content-center align-items-center homepage">
-    @if(count($homepages) > 0)
-        @foreach($homepages as $homepage)
+    @if(!empty($homepage))
+        <div class="row">
+            <div class="col">
+                <h1 class="display-3 text-center">{{$homepage->name}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p class="lead text-center">{{$homepage->job_title}}</p>
+            </div>
+        </div>
+        @if(!Auth::guest())
+            @if(Auth::user()->id == 1)
             <div class="row">
                 <div class="col">
-                    <h1 class="display-3 text-center">{{$homepage->name}}</h1>
+                    <a href="/homepage/{{$homepage->id}}/edit" class="btn btn-primary text-center">Edit</a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <p class="lead text-center">{{$homepage->job_title}}</p>
-                </div>
-            </div>
-            @if(!Auth::guest())
-                @if(Auth::user()->id == 1)
-                <div class="row">
-                    <div class="col">
-                        <a href="/homepage/{{$homepage->id}}/edit" class="btn btn-primary text-center">Edit</a>
-                    </div>
-                </div>
-                @endif
             @endif
-        @endforeach
+        @endif
     @endif
 </div>
 @endsection
