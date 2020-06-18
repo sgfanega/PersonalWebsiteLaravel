7d8a15d23ab105330464a7f0828aba5887d32a83
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-18 01:45:49 -0500

    Added index()

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Project;
+use DB;
 
 class ProjectsController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except'=> ['index']]);
+    }
+
+    /**
+     * Display a listing of the resource
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = DB::select('SELECT * FROM projects');
+
+        return view('projects.index')->with('projects', $projects);
+    }
 }
