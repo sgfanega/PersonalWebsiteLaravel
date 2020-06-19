cdc21482c584ee96dde4672a76d2b2d1dc673ee6
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-19 01:52:40 -0500

    added Back button

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create Project</h1>
-    {!! Form::open(['action'=>'ProjectsController@store', 'method'=>'POST', 'enctype'=>'multipart/form-data']) !!}
-    <div class="form-group">
-        {{Form::label('title', 'Title')}}
-        {{Form::text('title', '', ['class'=>'form-control', 'placeholder'=>'Title'])}}
+    <div class="container">
+        <a href="/projects" class="btn btn-dark">Back</a>
+        <hr>
+        <h1>Create Project</h1>
+        {!! Form::open(['action'=>'ProjectsController@store', 'method'=>'POST', 'enctype'=>'multipart/form-data']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', '', ['class'=>'form-control', 'placeholder'=>'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('language', 'Language')}}
+            <br>
+            {{Form::select('language', ['CSharp'=>'CSharp', 'PHP'=>'PHP', 'Java'=>'Java', 'Python'=>'Python', 'NoLogo'=>'Other'], 'NoLogo')}}
+        </div>
+        <div class="form-group">
+            {{Form::label('link', 'Link')}}
+            {{Form::text('link', '', ['class'=>'form-control', 'placeholder'=>'Link'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('description', 'Description')}}
+            {{Form::textarea('description', '', ['class'=>'form-control', 'placeholder'=>'Description'])}}
+        </div>
+        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+        {!! Form::close() !!}
     </div>
-    <div class="form-group">
-        {{Form::label('language', 'Language')}}
-        <br>
-        {{Form::select('language', ['CSharp'=>'CSharp', 'PHP'=>'PHP', 'Java'=>'Java', 'Python'=>'Python', 'NoLogo'=>'Other'], 'NoLogo')}}
-    </div>
-    <div class="form-group">
-        {{Form::label('link', 'Link')}}
-        {{Form::text('link', '', ['class'=>'form-control', 'placeholder'=>'Link'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('description', 'Description')}}
-        {{Form::textarea('description', '', ['class'=>'form-control', 'placeholder'=>'Description'])}}
-    </div>
-    {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
 @endsection
