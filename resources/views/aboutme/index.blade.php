2fd48629c683db993d8b4530ae840c1d5117d72c
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-23 18:58:21 -0500

    changed path of img

--- resources/views/aboutme/index.blade.php
+++ resources/views/aboutme/index.blade.php
@@ -16,7 +16,7 @@
     <hr>
     <div class="row">
         <div class="col">
-            <img src="/storage/portrait_image/{{$aboutme->portrait_image}}" alt="Self-Portrait" class="rounded">
+            <img src="{{ asset('/storage/portrait_image/'.$aboutme->portrait_image) }}" alt="Self-Portrait" class="rounded">
         </div>
     </div>
     <hr>
