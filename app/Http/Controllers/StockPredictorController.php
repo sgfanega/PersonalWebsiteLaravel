ffcf9b70fe6e8f1229ae133ae4ae94f19e95f793
Steve Fanega <steve.fanega.ii@gmail.com>
2020-06-29 18:27:56 -0500

    Testing the Charjs

--- app/Http/Controllers/StockPredictorController.php
+++ app/Http/Controllers/StockPredictorController.php
@@ -51,8 +51,37 @@ class StockPredictorController extends Controller
 
             return view('stockpredictor.index')->with('chartjs', $chartjs);
         }
-        
-        return view('stockpredictor.index');
+
+        $chartjs = app()->chartjs
+        ->name('lineChartTest')
+        ->type('line')
+        ->size(['width' => 400, 'height' => 200])
+        ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
+        ->datasets([
+            [
+                "label" => "My First dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [65, 59, 80, 81, 56, 55, 40],
+            ],
+            [
+                "label" => "My Second dataset",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [12, 33, 44, 44, 55, 23, 40],
+            ]
+        ])
+        ->options([]);
+
+        return view('stockpredictor.index')->with('chartjs', $chartjs);
     }
 
     /**
