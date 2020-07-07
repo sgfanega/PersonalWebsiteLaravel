324939b2c4c067e7807bfe50d4d6b92f9cf1ae94
Steve Fanega <steve.fanega.ii@gmail.com>
2020-07-07 18:25:18 -0500

    button alignment

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -25,10 +25,9 @@
                         <div class="row">
                             <div class="col d-flex justify-content-end align-items-center">
                                 <a href="/projects/{{$project->id}}/edit" class="btn btn-primary text-center">Edit</a>
-                                <br>
                                 {!! Form::open(['action'=>['ProjectsController@update', $project->id], 'method'=>'POST', 'enctype'=>'multipart/form-data']) !!}
                                 {{Form::hidden('_method', 'DELETE')}}
-                                {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+                                {{Form::submit('Delete', ['class'=>'btn btn-danger ml-1'])}}
                                 {!! Form::close() !!}
                             </div>
                         </div>
